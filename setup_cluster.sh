ad0690d921e21898634f7e3a22b2919d4b1af185
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-09-27 14:16:38 +0700

    Debug get all pod

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -41,7 +41,8 @@ kind create cluster --config=kind-config.yaml --image=kindest/node:${KIND_NODE_V
 kubectl create namespace vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm install vault hashicorp/vault --version=${VAULT_VERSION} --values=vault-values.yaml --namespace=vault
-sleep 10
+sleep 15
+kubectl get pod -o yaml --namespace=vault
 kubectl exec vault-0 --namespace=vault -- vault operator init -key-shares=1 -key-threshold=1 -format=json > init.json
 export UNSEAL_KEY=$(cat init.json | jq -r '.unseal_keys_b64[0]')
 kubectl exec vault-0 --namespace=vault -- vault operator unseal ${UNSEAL_KEY}
