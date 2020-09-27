0f1f35a255de6a65c6b81faba361715af81c3916
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-09-27 13:03:41 +0700

    Install Vault

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -7,34 +7,35 @@ export KNATIVE_VERSION=v0.15.0
 export ISTIO_VERSION=1.6.2
 export CERT_MANAGER_VERSION=v0.15.1
 export KFSERVING_VERSION=v0.4.0
-
-helm version
+export VAULT_VERSION=0.7.0
 
 # Provision KinD cluster
 kind create cluster --config=kind-config.yaml --image=kindest/node:${KIND_NODE_VERSION}
 
-# Install Knative
-kubectl apply --filename=https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
-kubectl apply --filename=https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
-kubectl wait deployment.apps/webhook --namespace=knative-serving --for=condition=available --timeout=300s
+# # Install Knative
+# kubectl apply --filename=https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
+# kubectl apply --filename=https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
+# kubectl wait deployment.apps/webhook --namespace=knative-serving --for=condition=available --timeout=300s
 
-# Install Istio
-curl --location https://git.io/getLatestIstio | sh -
-sudo install istio-${ISTIO_VERSION}/bin/istioctl /usr/bin/istioctl
-istioctl install --filename=istio-minimal-operator.yaml
+# # Install Istio
+# curl --location https://git.io/getLatestIstio | sh -
+# sudo install istio-${ISTIO_VERSION}/bin/istioctl /usr/bin/istioctl
+# istioctl install --filename=istio-minimal-operator.yaml
 
-kubectl apply --filename=https://github.com/knative/net-istio/releases/download/v0.15.0/release.yaml
-kubectl patch configmap/config-domain --namespace=knative-serving --type=merge --patch='{"data":{"127.0.0.1.xip.io":""}}'
+# kubectl apply --filename=https://github.com/knative/net-istio/releases/download/v0.15.0/release.yaml
+# kubectl patch configmap/config-domain --namespace=knative-serving --type=merge --patch='{"data":{"127.0.0.1.xip.io":""}}'
 
-# Install Cert Manager
-kubectl apply --validate=false --filename=https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
-kubectl wait deployment/cert-manager-webhook --namespace=cert-manager --for=condition=available --timeout=600s
+# # Install Cert Manager
+# kubectl apply --validate=false --filename=https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
+# kubectl wait deployment/cert-manager-webhook --namespace=cert-manager --for=condition=available --timeout=600s
 
-# Install KFServing
-kubectl apply --filename=https://raw.githubusercontent.com/kubeflow/kfserving/master/install/${KFSERVING_VERSION}/kfserving.yaml
-kubectl wait pod/kfserving-controller-manager-0 --namespace=kfserving-system --for=condition=ready --timeout=300s
+# # Install KFServing
+# kubectl apply --filename=https://raw.githubusercontent.com/kubeflow/kfserving/master/install/${KFSERVING_VERSION}/kfserving.yaml
+# kubectl wait pod/kfserving-controller-manager-0 --namespace=kfserving-system --for=condition=ready --timeout=300s
 
 # Install Vault
-
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm install vault hashicorp/vault --version=${VAULT_VERSION} --namespace=vault
+kubectl get all --namespace=vault
 
 set +ex
